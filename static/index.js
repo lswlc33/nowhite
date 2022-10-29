9c7050885dbcbd4eb62bc545f55df1b991861626
雪中明月 <lswlc33@qq.com>
2022-10-30 07:06:23 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -6,7 +6,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         
         const LANGUAGES = [
             { regex: /^zh\b/, lang: 'zh' },
-            { regex: /.*/, lang: 'zh'}
+            { regex: /.*/, lang: 'en'}
         ]
 
         const lang = LANGUAGES.find(l => l.regex.test(navigator.language)).lang
